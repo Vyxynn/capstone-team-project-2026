7f6699996a09e15f31da77c73b68154f0c9d030a
Ambrielle Gallegos <agalle553@west-mec.org>
2026-05-15 17:28:43 -0700

    added canvas animations and drawing

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,1 +1,36 @@
 // /src/scripts/canvas.js
+
+import { writeBodies, readBodies } from "./data";
+import { stepSimulation } from "./physics";
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// const interval = setInterval(() => {
+//     const bodies = readBodies();
+//     for (b in bodies) { 
+//         ctx.beginPath();
+//         ctx.arc(b.x, b.y, b.radius, 0, Math.PI * 2);
+//         ctx.fillStyle = b.color;
+//         ctx.fill();
+//     }
+// }, 1000);
+
+function animate() { 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const bodies = readBodies();
+    // if (bodies.length <= 0) return;
+    // updateBodies(bodies);
+    stepSimulation(bodies)
+
+    for (const b of bodies) {
+      ctx.beginPath();
+      ctx.arc(b.x, b.y, b.radius, 0, Math.PI * 2);
+      ctx.fillStyle = b.color;
+        ctx.fill();
+        console.log(b.x, b.y);
+    }
+    requestAnimationFrame(animate);
+}
+
+animate();
